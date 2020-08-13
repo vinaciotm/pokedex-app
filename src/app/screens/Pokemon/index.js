de64f275cba3d2ecaf415f7c902cc83636351306
Inácio Pires <viniciusinaciopires@gmail.com>
2020-08-13 20:41:28 -0300

    root Pokemon Details
    Busca detalhes do pokemon via redux saga

---
+++ src/app/screens/Pokemon/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+
+import {
+  Container,
+  Content,
+  Details,
+  DetailTitle,
+  DetailContent,
+  Divisor,
+} from "./styles";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemon } from "../../../redux/pokemon/action";
+
+import BackButton from "../../components/pokemon/BackButton";
+import Header from "../../components/pokemon/Header";
+import Stat from "../../components/pokemon/Stat";
+import Ability from "../../components/pokemon/Ability";
+
+const PokemonDetails = ({ route, navigation }) => {
+  const { id } = route.params;
+
+  const dispatch = useDispatch();
+  const pokemon = useSelector((state) => state.pokemon);
+
+  const Abilities = () =>
+    pokemon.data.abilities.map((ability, i) => (
+      <Ability key={i} name={ability} />
+    ));
+
+  useEffect(() => {
+    dispatch(getPokemon({ id }));
+  }, []);
+
+  return (
+    <Container>
+      <BackButton navigation={navigation} />
+
+      {pokemon.loaded && (
+        <Content>
+          <Header img={pokemon.data.img} id={id} name={pokemon.data.name} />
+
+          <Details>
+            <DetailTitle>Atributos</DetailTitle>
+
+            <DetailContent>
+              <Stat name="HP" value={pokemon.data.stats.hp} />
+              <Stat name="Ataque" value={pokemon.data.stats.attack} />
+              <Stat name="Defesa" value={pokemon.data.stats.defense} />
+              <Stat name="Velocidade" value={pokemon.data.stats.speed} />
+            </DetailContent>
+
+            <Divisor />
+
+            <DetailTitle>Habilidades</DetailTitle>
+
+            <DetailContent>
+              <Abilities />
+            </DetailContent>
+          </Details>
+        </Content>
+      )}
+    </Container>
+  );
+};
+
+export default PokemonDetails;
---
+++ src/app/screens/Pokemon/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components/native";
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  height: 100%;
+  background-color: #8bd674;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  padding-bottom: 30px;
+`;
+
+export const Details = styled.View`
+  flex: 1;
+  border-radius: 30px;
+  background-color: #fff;
+  padding: 30px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+`;
+
+export const DetailTitle = styled.Text`
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 42px;
+
+  color: #62b957;
+`;
+
+export const DetailContent = styled.View`
+  flex: 1;
+`;
+
+export const Divisor = styled.View`
+  width: 100%;
+  height: 20px;
+`;
