cafdec71661a97e3e08339395340604b1cf212fe
Inácio Pires <viniciusinaciopires@gmail.com>
2020-08-13 20:44:13 -0300

    pokemons component - header

---
+++ src/app/components/pokemons/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Title, Subtitle } from "./styles";
+
+const PokemonsListHeader = () => (
+  <Container>
+    <Title>Pokédex</Title>
+    <Subtitle>Todos pokemons na sua mão.</Subtitle>
+  </Container>
+);
+
+export default PokemonsListHeader;
---
+++ src/app/components/pokemons/Header/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 38px;
+
+  color: #17171b;
+`;
+
+export const Subtitle = styled.Text`
+  margin-top: 10px;
+
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #747476;
+`;
